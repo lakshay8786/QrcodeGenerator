ee584cdddc1d5d35f9b0e11ffec16343140fc0f8
lakshay8786 <flipylakshay790@gmail.com>
2022-05-02 22:09:53 +0530

    Updates

---
+++ precache-manifest.c0e561fd55e71783334e1faa69a26d68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "74417b5484e2b3998c28",
+    "url": "/QrcodeGenerator/static/css/main.d506d448.chunk.css"
+  },
+  {
+    "revision": "74417b5484e2b3998c28",
+    "url": "/QrcodeGenerator/static/js/main.74417b54.chunk.js"
+  },
+  {
+    "revision": "c471f40a4a3fbcbb1ed1",
+    "url": "/QrcodeGenerator/static/js/1.c471f40a.chunk.js"
+  },
+  {
+    "revision": "6d578b5870aaf25d4a1c",
+    "url": "/QrcodeGenerator/static/js/2.6d578b58.chunk.js"
+  },
+  {
+    "revision": "8e9c2081cb2cd9a3295a",
+    "url": "/QrcodeGenerator/static/js/runtime~main.8e9c2081.js"
+  },
+  {
+    "revision": "ef1a7a62c020e74102cd591a3ba64379",
+    "url": "/QrcodeGenerator/index.html"
+  }
+];
